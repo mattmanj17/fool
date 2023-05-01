4410c481463aaa519716d300a0ea848da57b6bda
Matthew Draper <draper_matthewjohn@yahoo.com>
2023-05-01 15:41:27 -0700

    classify ch

--- ctok/lex_state.c
+++ ctok/lex_state.c
@@ -1,31 +1,173 @@
 
-static bool Is_digit(char ch)
+typedef enum
 {
-	ch -= '0';
-	return ch >= 0 && ch < 10;
-}
+	CH_one_ch_tok,
+	CH_bslash,
+	CH_bang_or_caret,
+	CH_eq,
+	CH_star,
+	CH_colon,
+	CH_htag,
+	CH_vbar,
+	CH_plus,
+	CH_amp,
+	CH_minus,
+	CH_gt,
+	CH_lt,
+	CH_percent,
+	CH_eEpP,
+	CH_squote,
+	CH_dquote,
+	CH_L,
+	CH_U,
+	CH_u,
+	CH_fslash,
+	CH_dot,
+	CH_8,
+	CH_012345679,
+	CH_line_break,
+	CH_space,
+	CH_id_no_digit_no_eEpP,
+} CH;
 
-static bool Can_start_id(char ch)
+CH ch_map[128] =
 {
-	if (ch == '$') // allowed in ids as an extention :/
-		return true;
+	CH_one_ch_tok,			// NUL
+	CH_one_ch_tok,			// SOH
+	CH_one_ch_tok,			// STX
+	CH_one_ch_tok,			// ETX
+	CH_one_ch_tok,			// EOT
+	CH_one_ch_tok,			// ENQ
+	CH_one_ch_tok,			// ACK
+	CH_one_ch_tok,			// BEL
+	CH_one_ch_tok,			// BS
+	CH_space,				// HT
+	CH_line_break,			// LF
+	CH_space,				// VT
+	CH_space,				// FF
+	CH_line_break,			// CR
+	CH_one_ch_tok,			// SO
+	CH_one_ch_tok,			// SI
+	CH_one_ch_tok,			// DLE
+	CH_one_ch_tok,			// DC1
+	CH_one_ch_tok,			// DC2
+	CH_one_ch_tok,			// DC3
+	CH_one_ch_tok,			// DC4
+	CH_one_ch_tok,			// NAK
+	CH_one_ch_tok,			// SYN
+	CH_one_ch_tok,			// ETB
+	CH_one_ch_tok,			// CAN
+	CH_one_ch_tok,			// EM
+	CH_one_ch_tok,			// SUB
+	CH_one_ch_tok,			// ESC
+	CH_one_ch_tok,			// FS
+	CH_one_ch_tok,			// GS
+	CH_one_ch_tok,			// RS
+	CH_one_ch_tok,			// US
+	CH_space,				// SP
+	CH_bang_or_caret,		// !
+	CH_dquote,				// "
+	CH_htag,				// #
+	CH_id_no_digit_no_eEpP,	// $ (allowed in ids as extension :/)
+	CH_percent,				// %
+	CH_amp,					// &
+	CH_squote,				// '
+	CH_one_ch_tok,			// (
+	CH_one_ch_tok,			// )
+	CH_star,				// *
+	CH_plus,				// +
+	CH_one_ch_tok,			// ,
+	CH_minus,				// -
+	CH_dot,					// .
+	CH_fslash,				// /
+	CH_012345679,			// 0
+	CH_012345679,			// 1
+	CH_012345679,			// 2
+	CH_012345679,			// 3
+	CH_012345679,			// 4
+	CH_012345679,			// 5
+	CH_012345679,			// 6
+	CH_012345679,			// 7
+	CH_8,					// 8
+	CH_012345679,			// 9
+	CH_colon,				// :
+	CH_one_ch_tok,			// ;
+	CH_lt,					// <
+	CH_eq,					// =
+	CH_gt,					// >
+	CH_one_ch_tok,			// ?
+	CH_one_ch_tok,			// @
+	CH_id_no_digit_no_eEpP,	// A
+	CH_id_no_digit_no_eEpP,	// B
+	CH_id_no_digit_no_eEpP,	// C
+	CH_id_no_digit_no_eEpP,	// D
+	CH_eEpP,				// E
+	CH_id_no_digit_no_eEpP,	// F
+	CH_id_no_digit_no_eEpP,	// G
+	CH_id_no_digit_no_eEpP,	// H
+	CH_id_no_digit_no_eEpP,	// I
+	CH_id_no_digit_no_eEpP,	// J
+	CH_id_no_digit_no_eEpP,	// K
+	CH_L,					// L
+	CH_id_no_digit_no_eEpP,	// M
+	CH_id_no_digit_no_eEpP,	// N
+	CH_id_no_digit_no_eEpP,	// O
+	CH_eEpP,				// P
+	CH_id_no_digit_no_eEpP,	// Q
+	CH_id_no_digit_no_eEpP,	// R
+	CH_id_no_digit_no_eEpP,	// S
+	CH_id_no_digit_no_eEpP,	// T
+	CH_U,					// U
+	CH_id_no_digit_no_eEpP,	// V
+	CH_id_no_digit_no_eEpP,	// W
+	CH_id_no_digit_no_eEpP,	// X
+	CH_id_no_digit_no_eEpP,	// Y
+	CH_id_no_digit_no_eEpP,	// Z
+	CH_one_ch_tok,			// [
+	CH_bslash,				// \ (back slash)
+	CH_one_ch_tok,			// ]
+	CH_bang_or_caret,		// ^
+	CH_id_no_digit_no_eEpP,	// _
+	CH_one_ch_tok,			// `
+	CH_id_no_digit_no_eEpP,	// a
+	CH_id_no_digit_no_eEpP,	// b
+	CH_id_no_digit_no_eEpP,	// c
+	CH_id_no_digit_no_eEpP,	// d
+	CH_eEpP,				// e
+	CH_id_no_digit_no_eEpP,	// f
+	CH_id_no_digit_no_eEpP,	// g
+	CH_id_no_digit_no_eEpP,	// h
+	CH_id_no_digit_no_eEpP,	// i
+	CH_id_no_digit_no_eEpP,	// j
+	CH_id_no_digit_no_eEpP,	// k
+	CH_id_no_digit_no_eEpP,	// l
+	CH_id_no_digit_no_eEpP,	// m
+	CH_id_no_digit_no_eEpP,	// n
+	CH_id_no_digit_no_eEpP,	// o
+	CH_eEpP,				// p
+	CH_id_no_digit_no_eEpP,	// q
+	CH_id_no_digit_no_eEpP,	// r
+	CH_id_no_digit_no_eEpP,	// s
+	CH_id_no_digit_no_eEpP,	// t
+	CH_u,					// u
+	CH_id_no_digit_no_eEpP,	// v
+	CH_id_no_digit_no_eEpP,	// w
+	CH_id_no_digit_no_eEpP,	// x
+	CH_id_no_digit_no_eEpP,	// y
+	CH_id_no_digit_no_eEpP,	// z
+	CH_one_ch_tok,			// {
+	CH_vbar,				// |
+	CH_one_ch_tok,			// }
+	CH_one_ch_tok,			// ~
+	CH_one_ch_tok,			// DEL
+};
 
-	if (ch == '_')
-		return true;
-
-	// make lower case
-
-	ch |= 0x20;
-
-	// Check if letter
-
-	ch -= 'a';
-	return ch >= 0 && ch < 26;
-}
-
-static bool Extends_id(char ch)
+CH Classify_ch(char ch)
 {
-	return Is_digit(ch) || Can_start_id(ch);
+	if ((unsigned char)ch > 127)
+		return CH_one_ch_tok;
+
+	return ch_map[ch];
 }
 
 typedef enum 
@@ -64,7 +206,7 @@ typedef enum
 	LEX_STATE_EXIT,						// no inc char in tok, done with tok
 } LEX_STATE;
 
-static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
+static LEX_STATE Next_state(LEX_STATE cur_state, CH ch)
 {
 	switch (cur_state)
 	{
@@ -72,64 +214,63 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 		{
 			switch (ch)
 			{
-			case ' ': case '\t': 
-			case '\f': case '\v':
+			case CH_space:
 				return LEX_STATE_WS;
-
-			case '\r': case '\n':
+			case CH_line_break:
 				return LEX_STATE_LINES;
-
-			case '=': case '^': case '*': case '!':
+			case CH_eq:
+			case CH_star:
+			case CH_bang_or_caret:
 				return LEX_STATE_EQ_OR_EXIT;
-
-			case '.':
+			case CH_dot:
 				return LEX_STATE_DOT;
-			case '/':
+			case CH_fslash:
 				return LEX_STATE_FSLASH;
-			case 'u':
+			case CH_u:
 				return LEX_STATE_LOWER_U;
-			case 'U':
+			case CH_U:
 				return LEX_STATE_UPPER_U;
-			case 'L':
+			case CH_L:
 				return LEX_STATE_UPPER_L;
-			case '"':
+			case CH_dquote:
 				return LEX_STATE_IN_STR_LIT;
-			case '\'':
+			case CH_squote:
 				return LEX_STATE_IN_CHAR_LIT;
-			case '%':
+			case CH_percent:
 				return LEX_STATE_PERCENT;
-			case '<':
+			case CH_lt:
 				return LEX_STATE_LT;
-			case '>':
+			case CH_gt:
 				return LEX_STATE_GT;
-			case '#':
+			case CH_htag:
 				return LEX_STATE_HTAG;
-			case '&':
+			case CH_amp:
 				return LEX_STATE_AMP;
-			case '+':
+			case CH_plus:
 				return LEX_STATE_PLUS;
-			case '-':
+			case CH_minus:
 				return LEX_STATE_MINUS;
-			case ':':
+			case CH_colon:
 				return LEX_STATE_COLON;
-			case '|':
+			case CH_vbar:
 				return LEX_STATE_VBAR;
-			}
-
-			if (Is_digit(ch))
+			case CH_8:
+			case CH_012345679:
 				return LEX_STATE_PPNUN;
-
-			if (Can_start_id(ch))
+			case CH_eEpP:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_ID;
-
-			return LEX_STATE_DELAY_EXIT;
+			default:
+				return LEX_STATE_DELAY_EXIT;
+			}
 		}
 		break;
 
 	case LEX_STATE_PPNUM_SIGN:	
 		switch (ch)
 		{
-		case '-': case '+':
+		case CH_minus:
+		case CH_plus:
 			return LEX_STATE_PPNUN;
 		}
 
@@ -139,73 +280,107 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 		{
 			switch (ch)
 			{
-			case '.':
+			case CH_dot:
 				return LEX_STATE_PPNUN;
-			case 'e': case 'E': case 'p': case 'P':
+			case CH_eEpP:
 				return LEX_STATE_PPNUM_SIGN;
-			}
-
-			if (Extends_id(ch))
+			case CH_8:
+			case CH_012345679:
+			case CH_u:
+			case CH_U:
+			case CH_L:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_PPNUN;
-
-			return LEX_STATE_EXIT;
+			default:
+				return LEX_STATE_EXIT;
+			}
 		}
 
 	case LEX_STATE_LOWER_U:	
 		{
-			// reusing LEX_STATE_UPPER_U because it does what we want
+			// reusing LEX_STATE_UPPER_U in the CH_8 because it does what we want
 
-			if (ch == '8')
+			switch (ch)
+			{
+			case CH_8:
 				return LEX_STATE_UPPER_U; 
-
-			if (Extends_id(ch))
+			case CH_012345679:
+			case CH_u:
+			case CH_U:
+			case CH_L:
+			case CH_eEpP:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_ID;
-
-			return LEX_STATE_EXIT;
+			default:
+				return LEX_STATE_EXIT;
+			}
 		}
 		break;
 
 	case LEX_STATE_UPPER_U:	
 		{
-			if (ch == '"')
+			switch (ch)
+			{
+			case CH_dquote:
 				return LEX_STATE_IN_STR_LIT; 
-
-			if (Extends_id(ch))
+			case CH_8:
+			case CH_012345679:
+			case CH_u:
+			case CH_U:
+			case CH_L:
+			case CH_eEpP:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_ID;
-
-			return LEX_STATE_EXIT;
+			default:
+				return LEX_STATE_EXIT;
+			}
 		}
 		break;		
 
 	case LEX_STATE_UPPER_L:	
 		{
-			if (ch == '\'')
-				return LEX_STATE_IN_CHAR_LIT; 
-
-			if (ch == '"')
+			switch (ch)
+			{
+			case CH_squote:
+				return LEX_STATE_IN_CHAR_LIT;
+			case CH_dquote:
 				return LEX_STATE_IN_STR_LIT; 
-
-			if (Extends_id(ch))
+			case CH_8:
+			case CH_012345679:
+			case CH_u:
+			case CH_U:
+			case CH_L:
+			case CH_eEpP:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_ID;
-
-			return LEX_STATE_EXIT;
+			default:
+				return LEX_STATE_EXIT;
+			}
 		}
 		break;	
 
 	case LEX_STATE_ID:		
 		{
-			if (Extends_id(ch))
+			switch (ch)
+			{
+			case CH_8:
+			case CH_012345679:
+			case CH_u:
+			case CH_U:
+			case CH_L:
+			case CH_eEpP:
+			case CH_id_no_digit_no_eEpP:
 				return LEX_STATE_ID;
-
-			return LEX_STATE_EXIT;
+			default:
+				return LEX_STATE_EXIT;
+			}
 		}
 		break;
 
 	case LEX_STATE_WS:
 		switch (ch)
 		{
-		case ' ': case '\t': 
-		case '\f': case '\v': 
+		case CH_space: 
 			return LEX_STATE_WS;
 		default:
 			return LEX_STATE_EXIT;
@@ -214,9 +389,8 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_LINES:
 		switch (ch)
 		{
-		case ' ': case '\t': 
-		case '\f': case '\v':
-		case '\r': case '\n':
+		case CH_space:
+		case CH_line_break:
 			return LEX_STATE_LINES;
 		default: 
 			return LEX_STATE_EXIT;
@@ -225,8 +399,8 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_DOT:
 		switch (ch)
 		{
-		case '0': case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
+		case CH_8:
+		case CH_012345679:
 			return LEX_STATE_PPNUN;
 		default: 
 			return LEX_STATE_EXIT;
@@ -235,11 +409,11 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_FSLASH:	
 		switch (ch)
 		{
-		case '*':
+		case CH_star:
 			return LEX_STATE_IN_BLOCK_COMMENT;
-		case '/':
+		case CH_fslash:
 			return LEX_STATE_LINE_COMMENT;
-		case '=':
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -248,7 +422,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_STAR_IN_BLOCK_COMMENT:	
 		switch (ch)
 		{
-		case '/':
+		case CH_fslash:
 			return LEX_STATE_DELAY_EXIT;
 		}
 
@@ -257,7 +431,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_IN_BLOCK_COMMENT:	
 		switch (ch)
 		{
-		case '*':
+		case CH_star:
 			return LEX_STATE_STAR_IN_BLOCK_COMMENT;
 		default: 
 			return LEX_STATE_IN_BLOCK_COMMENT;
@@ -266,7 +440,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_LINE_COMMENT:	
 		switch (ch)
 		{
-		case '\r': case '\n':
+		case CH_line_break:
 			return LEX_STATE_EXIT;
 		default: 
 			return LEX_STATE_LINE_COMMENT;
@@ -275,11 +449,11 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_IN_STR_LIT:
 		switch (ch)
 		{
-		case '\r': case '\n':
+		case CH_line_break:
 			return LEX_STATE_EXIT;
-		case '"':
+		case CH_dquote:
 			return LEX_STATE_DELAY_EXIT;
-		case '\\':
+		case CH_bslash:
 			return LEX_STATE_ESC_STR_LIT;
 		default: 
 			return LEX_STATE_IN_STR_LIT;
@@ -288,7 +462,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_ESC_STR_LIT:	
 		switch (ch)
 		{
-		case '\r': case '\n':
+		case CH_line_break:
 			return LEX_STATE_EXIT;
 		default: 
 			return LEX_STATE_IN_STR_LIT;
@@ -297,11 +471,11 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_IN_CHAR_LIT:	
 		switch (ch)
 		{
-		case '\r': case '\n':
+		case CH_line_break:
 			return LEX_STATE_EXIT;
-		case '\'':
+		case CH_squote:
 			return LEX_STATE_DELAY_EXIT;
-		case '\\':
+		case CH_bslash:
 			return LEX_STATE_ESC_CHAR_LIT;
 		default: 
 			return LEX_STATE_IN_CHAR_LIT;
@@ -310,7 +484,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_ESC_CHAR_LIT:	
 		switch (ch)
 		{
-		case '\r': case '\n':
+		case CH_line_break:
 			return LEX_STATE_EXIT;
 		default: 
 			return LEX_STATE_IN_CHAR_LIT;
@@ -319,7 +493,9 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_PERCENT:
 		switch (ch)
 		{
-		case ':': case '=': case '>':
+		case CH_colon:
+		case CH_eq:
+		case CH_gt:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -328,9 +504,11 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_LT:	
 		switch (ch)
 		{
-		case '<':
+		case CH_lt:
 			return LEX_STATE_EQ_OR_EXIT;
-		case '%': case ':': case '=':
+		case CH_percent:
+		case CH_colon:
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -339,9 +517,9 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_GT:	
 		switch (ch)
 		{
-		case '>':
+		case CH_gt:
 			return LEX_STATE_EQ_OR_EXIT;
-		case '=':
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -350,7 +528,9 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_MINUS:
 		switch (ch)
 		{
-		case '>': case '-': case '=':
+		case CH_gt:
+		case CH_minus:
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -359,7 +539,8 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_AMP:	
 		switch (ch)
 		{
-		case '&': case '=':
+		case CH_amp:
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -368,7 +549,8 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_PLUS:
 		switch (ch)
 		{
-		case '+': case '=':
+		case CH_plus:
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -377,7 +559,8 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_VBAR:
 		switch (ch)
 		{
-		case '|': case '=':
+		case CH_vbar:
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -386,7 +569,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_HTAG:
 		switch (ch)
 		{
-		case '#':
+		case CH_htag:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -395,7 +578,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_COLON:	
 		switch (ch)
 		{
-		case '>':
+		case CH_gt:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -404,7 +587,7 @@ static LEX_STATE Next_state(LEX_STATE cur_state, char ch)
 	case LEX_STATE_EQ_OR_EXIT:
 		switch (ch)
 		{
-		case '=':
+		case CH_eq:
 			return LEX_STATE_DELAY_EXIT;
 		default: 
 			return LEX_STATE_EXIT;
@@ -429,7 +612,7 @@ int Len_leading_token(const char * str)
 		if (ch == '\0')
 			break;
 
-		LEX_STATE next_state = Next_state(state, ch);
+		LEX_STATE next_state = Next_state(state, Classify_ch(ch));
 
 		if (next_state == LEX_STATE_EXIT)
 			break;
@@ -443,8 +626,6 @@ int Len_leading_token(const char * str)
 		ch = str[0];
 	}
 
-#pragma warning(push)
-#pragma warning(disable : 4061)
 	switch (state)
 	{
 	case LEX_STATE_PERCENT:
@@ -472,7 +653,6 @@ int Len_leading_token(const char * str)
 	default:
 		break;
 	}
-#pragma warning(pop)
 
 	return (int)(str - str_begin);
 }
