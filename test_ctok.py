99da5e304b68f5e35cc56706501c7c49281884c2
Matthew Draper <draper_matthewjohn@yahoo.com>
2023-10-29 12:32:11 -0700

    rewrite the thing :/

--- test_ctok.py
+++ test_ctok.py
@@ -2,90 +2,64 @@
 import os
 import subprocess
 import concurrent.futures
+from pathlib import Path
 import filecmp
 
-from pathlib import Path
-
-def clang_path():
-	return "3rd_party\\llvm-project\\build\\Release\\bin\\clang.exe"
-
-def ctok_path():
-	return "build\\exe\\ctok.exe"
-
-def to_out_path(in_path, ext):
-	out_path = os.path.join("test/ctok/" + ext, in_path)
-	out_path += "." + ext
-	return out_path
-
-def bake_path(pth):
-	return os.path.normpath(os.path.join(os.getcwd(), pth))
-
-def input_paths():
-	for root, dirs, files in os.walk("3rd_party/llvm-project", topdown=True):
-		if root == "3rd_party/llvm-project":
-			dirs.remove('build')
-			dirs.remove('.git')
-		for fname in files:
-			in_path = os.path.join(root, fname)
-			yield in_path
-
-def run_clang(input_path):
-	out_path = to_out_path(input_path, "clang_tok")
-
-	input_path = bake_path(input_path)
-	out_path = bake_path(out_path)
-
-	cmd = "-dump-raw-tokens"
-	warns = "-Wno-unicode-homoglyph"
-	invoke_clang = f"{clang_path()} -cc1 {warns} {cmd} -std=c11 -x c" # specifically targeting c11
+def run_clang(in_path, out_path):
+	clang_path = os.path.abspath("3rd_party/llvm-project/build/Release/bin/clang.exe")
 	Path(os.path.dirname(out_path)).mkdir(parents=True, exist_ok=True)
 	with open(out_path, "wb") as out_f:
 		subprocess.run(
-			invoke_clang + " \"" + input_path + "\"", 
+			f'{clang_path} -cc1 -dump-raw-tokens -std=c11 -x c {in_path}', 
 			stderr=out_f)
 	print(out_path)
 		
-def gen_clang_tok():
-	with concurrent.futures.ThreadPoolExecutor(max_workers=19) as executor:
-		for input_path in input_paths():
-			executor.submit(run_clang, input_path)
-
-def run_ctok(input_path):
-	out_path = to_out_path(input_path, "ctok_tok")
-
-	input_path = bake_path(input_path)
-	out_path = bake_path(out_path)
-
+def run_ctok(in_path, out_path):
+	ctok_path = os.path.abspath("build/exe/ctok.exe")
 	Path(os.path.dirname(out_path)).mkdir(parents=True, exist_ok=True)
 	with open(out_path, "wb") as out_f:
 		subprocess.run(
-			ctok_path() + " -raw " + " \"" + input_path + "\"", 
+			f'{ctok_path} -raw {in_path}', 
 			stdout=out_f)
 	print(out_path)
 
-def gen_ctok_tok():	
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		for input_path in input_paths():
-			executor.submit(run_ctok, input_path)
+# list cases (input path, clang output path, ctok output path)
 
-def compare():
-	for input_path in input_paths():
-		out_path_ctok = to_out_path(input_path, "ctok_tok")
-		out_path_clang = to_out_path(input_path, "clang_tok")
+input_dir = os.path.abspath("test/ctok/input/llvm-project")
+clang_out_dir = os.path.abspath("test/ctok/output/clang")
+ctok_out_dir = os.path.abspath("test/ctok/output/ctok")
 
-		out_path_ctok = bake_path(out_path_ctok)
-		out_path_clang = bake_path(out_path_clang)
+cases = []
+for root, _, files in os.walk(input_dir):
+	for fname in files:
+		in_path = os.path.join(root, fname)
+		relpath = os.path.relpath(in_path, input_dir)
+		
+		clang_out_path = os.path.join(clang_out_dir, relpath)
+		clang_out_path += ".tokens"
+		
+		ctok_out_path = os.path.join(ctok_out_dir, relpath)
+		ctok_out_path += ".tokens"
 
-		print(input_path)
-		if not filecmp.cmp(out_path_clang, out_path_ctok):
-			print('failed!')
-			print('opening diff in beyond comapre (clang on left, ctok on right)')
-			
-			# ' /fv="Hex Compare" '
-			bcomapre_cmd = f'bcompare "{out_path_clang}" "{out_path_ctok}"'
-			print(bcomapre_cmd)
-			subprocess.run(bcomapre_cmd)
-			break
+		cases.append((in_path, clang_out_path, ctok_out_path))
 
-if __name__ == "__main__":
-	compare()
+# ensure clang and ctok output
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=19) as executor:
+	for in_path, clang_out_path, ctok_out_path in cases:
+		if not os.path.isfile(clang_out_path):
+			executor.submit(run_clang, in_path, clang_out_path)
+		if not os.path.isfile(ctok_out_path):
+			executor.submit(run_ctok, in_path, ctok_out_path)
+
+# count failures
+
+num_files = 0
+num_failed = 0
+for in_path, clang_out_path, ctok_out_path in cases:
+	print(in_path)
+	num_files += 1
+	if not filecmp.cmp(clang_out_path, ctok_out_path):
+		num_failed += 1
+
+print(f'{num_failed} of {num_files} failed')
