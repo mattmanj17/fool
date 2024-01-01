7cf97b355443b523fa4aff531b69cd2421b59b0f
Matthew Draper <draper_matthewjohn@yahoo.com>
2024-01-01 11:21:29 -0800

    remove some test files, dont generate the 'spew' test files any more, and remove some skips that have been removed from the corpus

--- test.py
+++ test.py
@@ -74,42 +74,11 @@ def setup_tests():
 def setup_test_input():
 	os.mkdir('untracked/test/input')
 	copy_raw_corpus()
-	copy_test_files()
-	generate_test_input()
 	scrub_test_input()
 
 def copy_raw_corpus():
 	print("copy_corpus")
 	shutil.copytree("untracked/fool_corpus/ctok_raw/","untracked/test/input/fool_corpus/ctok_raw")
-
-def copy_test_files():
-	print("copy other test files")
-	shutil.copytree("test","untracked/test/input/test")
-
-def generate_test_input():
-	# 'chars' is a carefully chosen subset of characters, 
-	#  designed to exersise interesting code paths in the lexer,
-	#  especially hitting an EOF in different states.
-
-	# bug! this will not hit the full UCN code paths...
-	#  need input at least 6 chars long for that
-
-	chars = b"\"\\\n\x20'8uULE0_.-*/"
-	out_dir = "untracked/test/input/spew"
-
-	if not os.path.exists(out_dir):
-		os.makedirs(out_dir, exist_ok=True)
-
-	findex = 0
-	for result in itertools.product(chars, repeat=4):
-		file_name = f"{out_dir}/spew_{findex}.txt"
-		
-		if (findex % 1000) == 0:
-			print(f'gen spew {file_name}')
-		
-		with open(file_name, "wb") as binary_file:
-			binary_file.write(bytes(result))
-		findex += 1
 
 def scrub_test_input():
 	print("scrub")
@@ -183,68 +152,14 @@ def run_tests():
 
 		# misc files we don't even care about
 
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/ancient-c-compilers-6e23e03d7b341b9e7493f069442c6a5c20cadd83/primeval-c/Caldera-license.pdf'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/cpython-3.12.0/Lib/test/xmltestdata/c14n-20/README'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/cpython-3.12.0/Misc/NEWS.d/next/C API/README.rst'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/cpython-3.12.0/Misc/NEWS.d/next/Core and Builtins/README.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/contrib/unicode/unicode-license.txt'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/contrib/header-tools/README'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/rust/README.md'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/linux-6.6/Documentation/translations/zh_CN/admin-guide/README.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/linux-6.6/Documentation/translations/zh_CN/process/license-rules.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/linux-6.6/Documentation/translations/zh_TW/process/license-rules.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/linux-6.6/Documentation/translations/zh_TW/admin-guide/README.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/lldb/third_party/Python/module/pexpect-4.6/README.rst'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/openmp/libompd/gdb-plugin/README.txt'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/obs-studio-30.0.0/plugins/obs-qsv11/QSV11-License-Clarification-Email.txt'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/open-watcom-v2-2023-12-01-Build/distrib/wc11/jreadme.110'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/raylib-5.0/examples/README.md'),
-
-		# garbage utf8 in id, which we treat differently than clang
-
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/Wbidi-chars-17.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/Winvalid-utf8-3.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/ucnid-1-utf8.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/ucnid-2-utf8.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/Winvalid-utf8-11.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/Winvalid-utf8-10.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/Winvalid-utf8-9.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/Winvalid-utf8-12.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/FixIt/fixit-unicode.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Preprocessor/utf8-allowed-chars.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Preprocessor/ucn-allowed-chars.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/SemaCXX/format-strings-0x.cpp'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/libcxx/benchmarks/std_format_spec_string_unicode.bench.cpp'),
-
-		# named UCN, which we do not support
-
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/Wbidi-chars-25.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/named-universal-char-escape-1.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/named-universal-char-escape-2.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/named-universal-char-escape-6.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/named-universal-char-escape-5.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/c-c++-common/cpp/named-universal-char-escape-7.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/named-universal-char-escape1.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/g++.dg/cpp23/named-universal-char-escape2.C'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/CXX/drs/dr26xx.cpp'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/FixIt/fixit-unicode-named-escape-sequences.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Preprocessor/ucn-pp-identifier.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Sema/ucn-identifiers.c'),
-
-		# both garbage in ids AND named ucn
-
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Lexer/unicode.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Parser/cxx11-user-defined-literals.cpp'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/libcxx/test/std/utilities/format/format.functions/escaped_output.unicode.pass.cpp'),
 
 		# utf8 BOM. we generate the same tokens, but have different col numbers on the first line (on purpose)
 
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/gcc.dg/cpp/pr33415.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/gcc-releases-gcc-13.2.0/gcc/testsuite/gcc.dg/diagnostic-input-utf8-bom.c'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang/test/Frontend/Inputs/rewrite-includes-bom.h'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/clang-tools-extra/clang-tidy/cppcoreguidelines/NoMallocCheck.h'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/llvm/include/llvm/ADT/GenericUniformityImpl.h'),
-		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/llvm-project-llvmorg-17.0.3/llvm/include/llvm/Transforms/Scalar/SROA.h'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/raylib-5.0/projects/VS2019-Android/raylib_android/raylib_android.NativeActivity/android_native_app_glue.h'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/raylib-5.0/projects/VS2019-Android/raylib_android/raylib_android.NativeActivity/android_native_app_glue.c'),
 		os.path.abspath('untracked/test/input/fool_corpus/ctok_raw/raylib-5.0/projects/VS2019-Android/raylib_android/raylib_android.NativeActivity/main.c'),
